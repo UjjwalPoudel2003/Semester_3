38db3e7637fbc0496e070a857dc1cb4e9f23e4e8
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-09-19 21:04:45 -0400

    modified json package

--- web_application_development/week_1/firstApp/index.js
+++ web_application_development/week_1/firstApp/index.js
@@ -6,26 +6,41 @@
 //     response.end();
 // }).listen(3000, () => console.log("Server is running on port 3000"));
 
-const connect = require('connect');
-const app = connect();
+// const connect = require('connect');
+const express = require('express');
+// const app = connect();
+const app = express();
 
-const log = (request, response, next) => {
-    console.log(request.url);
-    next();
-}
+// Purpose of the middleware is to create route
+app.use('/', (request, response) => {
+    response.send('hello world');
+});
 
-const helloFunction = (request, response, next) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.end("<h1>Hello World!</h1>");
-}
+app.listen(8081);
 
-const byeFunction = (request, response, next) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.end("<h1>bye World!</h1>");
-}
+// const log = (request, response, next) => {
+//     console.log(request.url);
+//     next();
+// }
 
-app.use(log);
-app.use('/hello', helloFunction);
-app.use('/bye', byeFunction);
+// const helloFunction = (request, response, next) => {
+//     response.setHeader('Content-Type', 'text/html');
+//     response.end("<h1>Hello World!</h1>");
+// }
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+// const byeFunction = (request, response, next) => {
+//     response.setHeader('Content-Type', 'text/html');
+//     response.end("<h1>bye World!</h1>");
+// }
+
+// // Home page for the website
+// const homePage = (request, response, next) => {
+//     response.setHeader('Content-Type', 'text/html');
+//     response.end("<h1>Home Page</h1>");
+// }
+
+// app.use(log);
+// app.use('/hello', helloFunction);
+// app.use('/bye', byeFunction);
+
+// app.listen(3000, () => console.log("Server is running on port 3000"));
